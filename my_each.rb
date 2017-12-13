64529079589c6845081f32dc35e93d2cf11efdb8
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-13 16:53:54 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arguement)
+  if block_given?
+  i = 0
+while i < arguement.length
+  yield(arguement[i])
+  i += 1
 end
+arguement
+else
+  puts "Did you check your block?"
+end
+end
+
